2b9118b28d229b25409c86adc53e8eaa25955853
AliceBorges <al_borges100@hotmail.com>
2025-10-12 18:54:16 -0300

    new login adm

--- client/cypress/e2e/login.spec.cy.ts
+++ client/cypress/e2e/login.spec.cy.ts
@@ -1,6 +1,6 @@
 describe('Login com sucesso', () => {
   it('Deve fazer login com um usuário válido', () => {
-    cy.visit('http://localhost:3000/heroes')
+    cy.visit('/')
     cy.get('li > .undefined').click()
     cy.get('[data-cy="email"]').type('test@test.com')
     cy.get('[data-cy="password"]').type('test123')
@@ -10,7 +10,7 @@ describe('Login com sucesso', () => {
 
 describe('Tentar fazer login com credenciais inválidas', () => {
   it('Deve exibir uma mensagem de erro ao fazer login com credenciais inválidas', () => {
-    cy.visit('http://localhost:3000/heroes')
+    cy.visit('/')
     cy.get('li > .undefined').click()
     cy.get('[data-cy="email"]').type('test@teste.com')
     cy.get('[data-cy="password"]').type('test123')
@@ -21,7 +21,7 @@ describe('Tentar fazer login com credenciais inválidas', () => {
 
 describe('Após realizar login dar um like em um hero', () => {
   it('Deve logar com sucesso, clicar no like de um hero a quantidade deve aumentar', () => {
-    cy.visit('http://localhost:3000/heroes')
+    cy.visit('/')
     cy.get('li > .undefined').click()
     cy.get('[data-cy="email"]').type('test@test.com')
     cy.get('[data-cy="password"]').type('test123')
@@ -32,12 +32,30 @@ describe('Após realizar login dar um like em um hero', () => {
 
 describe('Após realizar login dar um hire em um hero', () => {
   it('Deve logar com sucesso, clicar em hire de um hero e confirmar', () => {
-    cy.visit('http://localhost:3000/heroes')
+    cy.visit('/')
     cy.get('li > .undefined').click()
     cy.get('[data-cy="email"]').type('test@test.com')
     cy.get('[data-cy="password"]').type('test123')
     cy.get('button.bg-blue-700').click()
     cy.get("[data-cy='money']").eq(1).click()
     cy.get('.text-white').click()
+  })
+})
+
+describe('Após realizar login ADM criar um novo hero', () => {
+  it('Deve logar com sucesso, e criar um novo hero perfil', () => {
+    cy.visit('/')
+    cy.get('li > .undefined').click()
+    cy.get('[data-cy="email"]').type('admin@test.com')
+    cy.get('[data-cy="password"]').type('test123')
+    cy.get('button.bg-blue-700').click()
+    cy.get('a > .undefined').click()
+    cy.get('[data-cy="nameInput"]').type('New Hero')
+    cy.get('[data-cy="priceInput"]').type('10')
+    cy.get('[data-cy="fansInput"]').type('100')
+    cy.get('[data-cy="savesInput"]').type('50')
+    cy.get('[data-cy="powersSelect"]').select('Invisibility')
+    cy.get('[novalidate=""] .bg-blue-700').click()
+    cy.contains('[data-cy="name"]', 'New Hero', { timeout: 5000 })
   })
 })
